056793b65462946e07381208017865d352859d70
zkldi <ktchidev@gmail.com>
2021-10-14 14:53:50 +0100

    Add tachi logo picker

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,10 @@ pnpm install && pnpm build
 
 mkdir -p /data/zkldixyz/playground/distro
 cp -r public/. /data/zkldixyz/playground/distro
+
+# LOGO PICKER
+
+cd ..
+
+mkdir -p /data/zkldixyz/playground/tachi-logo
+cp tachi-logo/index.html /data/zkldixyz/playground/tachi-logo/index.html

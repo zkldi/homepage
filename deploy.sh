d9753bfdf6a9043407dcd0cfcd22ed3e093eb9fe
zkldi <20380519+zkldi@users.noreply.github.com>
2022-04-06 21:45:00 +0100

    Add a plainsong deploy script

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,25 @@ cd ..
 mkdir -p /data/zkldixyz/playground/tachi-logo
 cp tachi-logo/index.html /data/zkldixyz/playground/tachi-logo/index.html
 
+# PLAINSONG
+
+mkdir -p /data/plainsong
+
+(
+	cd plainsong/client || exit
+	pnpm install && pnpm build
+
+	cp dist/* /data/plainsong
+)
+
+(
+	cd plainsong/server || exit
+	cargo build --release
+	mv ./target/release/plainsong-server /usr/local/bin/plainsong-server
+	mv plainsong-server.service /lib/systemd/system
+	systemctl start plainsong-server
+)
+
 # static
 
 for k in "flow" "perhaps"
